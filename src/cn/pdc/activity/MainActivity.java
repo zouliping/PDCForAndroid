30a58be6cd09976b48cba76e56816077d386b251
zouliping <zouliping007@163.com>
2014-02-26 20:50:25 +0800

    添加登出功能；并将原本应用程序界面呈现的黑色变为白色

--- src/cn/pdc/activity/MainActivity.java
+++ src/cn/pdc/activity/MainActivity.java
@@ -1,6 +1,13 @@
 package cn.pdc.activity;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.SharedPreferences.Editor;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.ImageView;
 import android.widget.TextView;
 import cn.jpush.android.api.InstrumentedActivity;
 import cn.jpush.android.api.JPushInterface;
@@ -9,6 +16,9 @@ import cn.pdc.utils.AppUtil;
 import cn.pdc.utils.Util;
 
 public class MainActivity extends InstrumentedActivity {
+
+	private Context mContext = MainActivity.this;
+	private ImageView iv_logout;
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
@@ -39,6 +49,22 @@ public class MainActivity extends InstrumentedActivity {
 		String versionName = Util.GetVersion(getApplicationContext());
 		TextView mVersion = (TextView) findViewById(R.id.tv_version);
 		mVersion.setText("Version: " + versionName);
+
+		iv_logout = (ImageView) findViewById(R.id.logout_btn);
+		iv_logout.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				SharedPreferences data = mContext.getSharedPreferences("data",
+						0);
+				Editor editor = data.edit();
+				editor.clear();
+				editor.commit();
+
+				Intent i = new Intent(mContext, LoginActivity.class);
+				startActivity(i);
+			}
+		});
 
 	}
 }
