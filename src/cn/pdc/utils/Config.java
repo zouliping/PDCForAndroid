0ab2a089fc2cddd08e427f28661aa7aa49cc9ebf
zouliping <zouliping007@163.com>
2014-05-13 22:56:52 +0800

    修复登录bug

--- src/cn/pdc/utils/Config.java
+++ src/cn/pdc/utils/Config.java
@@ -4,7 +4,7 @@ public class Config {
 	public static final String WEICIYUAN_PACKAGE_NAME = "org.qii.weiciyuan";
 	public static final String WEICIYUAN_ACTIIVTY_NAME = "org.qii.weiciyuan.ui.send.WriteWeiboActivity";
 
-	public static final String IP = "http://192.168.0.126:9000";
+	public static final String IP = "http://172.17.214.191:9000";
 
 	// login uri
 	public static final String LOGIN = IP + "/user/login";
